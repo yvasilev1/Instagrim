5c9ba63ec555d1379b36a575deee641cac14b325
Yulian <Yulian@Yuli>
2015-10-19 23:37:32 +0100

    new commit

--- src/main/java/uk/ac/dundee/computing/aec/instagrim/stores/LoggedIn.java
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/stores/LoggedIn.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package uk.ac.dundee.computing.aec.instagrim.stores;
 
 /**
@@ -11,29 +10,35 @@ package uk.ac.dundee.computing.aec.instagrim.stores;
  * @author Administrator
  */
 public class LoggedIn {
-    boolean logedin=false;
-    String Username=null;
-    public void LogedIn(){
-        
+
+    boolean logedin = false;
+    String Username = null;
+
+    public void LogedIn() {
+
     }
-    
-    public void setUsername(String name){
-        this.Username=name;
+
+    public void setUsername(String name) {
+        this.Username = name;
     }
-    public String getUsername(){
+
+    public String getUsername() {
         return Username;
     }
-    public void setLogedin(){
-        logedin=true;
+
+    public void setLogedin() {
+        logedin = true;
     }
-    public void setLogedout(){
-        logedin=false;
+
+    public void setLogedout() {
+        logedin = false;
     }
-    
-    public void setLoginState(boolean logedin){
-        this.logedin=logedin;
+
+    public void setLoginState(boolean logedin) {
+        this.logedin = logedin;
     }
-    public boolean getlogedin(){
+
+    public boolean getlogedin() {
         return logedin;
     }
 }
---
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/stores/Profile.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.dundee.computing.aec.instagrim.stores;
+
+/**
+ *
+ * @author Yulian
+ */
+public class Profile {
+
+    public void Profile() {
+
+    }
+
+    String FirstName = null;
+    String LastName = null;
+    String Email = null;
+
+    public void setFirstName(String firstName) {
+        this.FirstName = firstName;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.LastName = lastName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setEmail(String email) {
+        this.Email = email;
+    }
+
+    public String getEmail() {
+        return Email;
+
+    }
+
+}
